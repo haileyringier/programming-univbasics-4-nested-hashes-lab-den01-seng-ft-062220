d7ce673a5725b6bbcefdf128603e878cc8a02873
Hailey Ringier <haileyringier@outlook.com>
2020-06-13 20:53:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,9 +78,6 @@ programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -96,7 +93,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
